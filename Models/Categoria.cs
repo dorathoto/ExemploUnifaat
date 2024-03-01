d6feecd5444817cf25258cb04a5653629e8c2c08
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2024-03-01 18:28:36 -0300

    Models

---
+++ Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exemplo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        [MaxLength(100)]
+        [Required(ErrorMessage = "O nome da categoria é obrigatório")]
+        public string Nome { get; set; }
+
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exemplo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "O nome do produto é obrigatório")]
+        public string Nome { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+    }
+}
